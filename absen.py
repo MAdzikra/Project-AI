72d73d9658ec3b39cfdaa32df9100fedd7058834
candr723 <candra.wibawa74@gmail.com>
2024-06-14 13:33:31 +0700

    hapus baris kode yang ga perlu

--- absen.py
+++ absen.py
@@ -135,16 +135,6 @@ def recognize_and_attend(model_path='models/face_recognition_model.xml', output_
     camera.release()
     cv2.destroyAllWindows()
 
-    if recognized:
-        for name, npm in attendance_set:
-            with open(output_csv, 'a', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([name, npm, time.strftime("%Y-%m-%d %H:%M:%S")])
-                # Display welcome message
-                messagebox.showinfo("Welcome", f"Selamat datang {name} NPM {npm}!!")
-    else:
-        messagebox.showinfo("Unrecognized", "Wajah tidak dikenali!!")
-
 # Functions to connect with GUI buttons
 def register():
     name = entry_name.get()
